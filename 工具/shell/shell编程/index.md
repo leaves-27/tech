45294676eec48944335ac88f667d2a9379a42511
migu <migu@fanhaoyue.com>
2020-07-09 14:14:55 +0800

    11

--- 工具/shell/shell编程/index.md
+++ 工具/shell/shell编程/index.md
@@ -1,2 +1,5 @@
 # 关键字说明
 #!:用来指定解释此脚本的shell程序
+
+# shell模块化
+https://blog.51cto.com/atong/1912179
--- 工具/shell/shell编程/test.sh
+++ 工具/shell/shell编程/test.sh
@@ -1,6 +1,25 @@
-#!/bin/env bash
-START=$(date +%s)
-envtype=${1:dev}
+#!/bin/zsh
+# START=$(date +%s)
+# envtype=${1:dev}
 
-echo $START
-echo $envtype
+# echo $START
+# echo $envtype
+
+# --------------------------------------
+# 练习1：创建用户及文件目录
+# 练习2：对用户或目录文件进行操作
+# 练习3： 
+# --------------------------------------
+
+function createUserAndFile(){
+    echo "创建用户和文件"
+}
+
+function editFile(){
+    echo "编辑文件"
+}
+
+function editUser(){
+    echo "编辑文件"
+}
+
